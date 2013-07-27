60df5daff045e618d9dca9f19790a7c5fe58477f
kbaba1001 <kbaba1001@gmail.com>
2013-07-27 13:19:04 +0900

    Add setting file

--- OpenWithVim.py
+++ OpenWithVim.py
@@ -4,11 +4,12 @@ import subprocess
 
 class OpenWithVimCommand(sublime_plugin.WindowCommand):
     def run(self):
-        terminal = "gnome-terminal"
-        option = "--command"
-        vim = "/usr/bin/vim"
-        path = None
+        setting = sublime.load_settings("OpenWithVim.sublime-settings")
+        terminal = setting.get("open_with_vim_terminal")
+        terminal_option = setting.get("open_with_vim_terminal_option")
+        vim_path = setting.get("open_with_vim_path")
 
+        path = None
         if self.window.active_view():
             path = "\"" + self.window.active_view().file_name() + "\""
         else:
