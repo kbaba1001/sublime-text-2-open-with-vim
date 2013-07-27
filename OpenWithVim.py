a41744a662fc1593fa2b8cd691a4e379ad5f2046
kbaba1001 <kbaba1001@gmail.com>
2013-07-27 13:19:52 +0900

    set current_line to vim
    vimを開いたときにSublimeTextのカーソル位置を渡すようにした

--- OpenWithVim.py
+++ OpenWithVim.py
@@ -2,7 +2,14 @@ import sublime
 import sublime_plugin
 import subprocess
 
+
 class OpenWithVimCommand(sublime_plugin.WindowCommand):
+
+    def current_line(self, view):
+        row, col = view.rowcol(view.sel()[0].begin())
+        row += 1
+        return row
+
     def run(self):
         setting = sublime.load_settings("OpenWithVim.sublime-settings")
         terminal = setting.get("open_with_vim_terminal")
@@ -11,10 +18,12 @@ class OpenWithVimCommand(sublime_plugin.WindowCommand):
 
         path = None
         if self.window.active_view():
-            path = "\"" + self.window.active_view().file_name() + "\""
+            path = self.window.active_view().file_name()
         else:
             sublime.error_message(__name__ + ": No file to open.")
             return
 
-        args = [terminal, option, vim + " " + path]
-        subprocess.Popen(args)
+        line_num = self.current_line(self.window.active_view())
+
+        command = [terminal, terminal_option, vim_path + ' +' + str(line_num) + ' ' + path]
+        subprocess.Popen(command)
